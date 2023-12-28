1878669a5f1fbeed46d07f9537d9b9c52b069504
notmyfault <fallenangelhd06@gmail.com>
2023-12-28 23:27:51 +0000

    Update Created Content class with gets and sets

--- BackEnd/Content.java
+++ BackEnd/Content.java
@@ -5,4 +5,26 @@ import BackEnd.Listas.ListAluno;
 public class Content {
     private ListAluno presencas;
     private String descricao;
+
+    public Content(){
+        presencas = new ListAluno();
+        descricao = "";
+    }
+    public Content(ListAluno presencas,String descricao){
+        this.presencas = presencas;
+        this.descricao = descricao;
+    }
+    public void setPresencas(ListAluno presencas){
+        this.presencas = presencas;
+    }
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+    public ListAluno getPresencas(){
+        return presencas;
+    }
+    public String Descricao(){
+        return descricao;
+    }
+
 }
